e176fc95d18e72a9bc2e8d6fd4bb82a12da315af
Gyuri Horak <dyuri@horak.hu>
2021-05-26 19:40:18 +0200

    1.0.5 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except (IOError, OSError):
 
 setup(
     name="xontrib-pyenv",
-    version='1.0.4',
+    version='1.0.5',
     url='https://github.com/dyuri/xontrib-pyenv',
     license='MIT',
     author='Gyuri Horák',
