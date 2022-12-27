5ee962255c84fdf35f4a626e9b112905ee44f1dc
eugenesvk <eugenesvk@users.noreply.github.com>
2022-12-28 03:19:50 +0700

    pyenv: set environment directly without calling pyenv

--- xontrib/pyenv.py
+++ xontrib/pyenv.py
@@ -5,25 +5,29 @@ import os
 import subprocess
 from .langenv_common import get_bin, create_alias
 from xonsh.lib      import subprocess as subproc
+from builtins        import __xonsh__    # XonshSession (${...} is '__xonsh__.env')
 
 __all__ = ()
 
-PYENV = get_bin('pyenv')
+base  = 'pyenv'
+PYENV = get_bin(base)
 if PYENV:
+    # Set environment
+    envx = __xonsh__.env
+    Home = envx.get("HOME")
+    PYENV_ROOT = envx.get("PYENV_ROOT")
+    if not PYENV_ROOT:
+        PYENV_ROOT = f"{Home}/.{base}"
+    envx.get("PATH").add(f'{PYENV_ROOT}/shims', front=True) # prepend shims to PATH
+    envx["PYENV_SHELL"] = "Python"
+
     PYENV_VENV = None
 
-    cmd_env    = [PYENV,           'init','-','--no-rehash']
     cmd_venv   = [PYENV,'virtualenv-init','-']
-    PYENV_ENV  = subprocess.run(cmd_env ,capture_output=True,encoding="UTF-8").stdout
     PYENV_VENV = subprocess.run(cmd_venv,capture_output=True,encoding="UTF-8").stdout
-
-    # init pyenv
-    cmd_pyenv = ['source-bash','-n','--suppress-skip-message',PYENV_ENV]
-    subproc.run(cmd_pyenv)
-
     if PYENV_VENV:
         # init pyenv-virtualenv
         cmd_venv = ['source-bash','-n','--suppress-skip-message',PYENV_VENV]
         subproc.run(cmd_venv)
 
-    create_alias("pyenv", PYENV, PYENV_ENV)
+    create_alias(base, PYENV)
