8ac43373c5f45c71ae4206b17985abb3e0a2c856
eugenesvk <eugenesvk@users.noreply.github.com>
2022-12-28 04:23:42 +0700

    nodenv: set environment directly without calling nodenv

--- xontrib/nodenv.py
+++ xontrib/nodenv.py
@@ -3,16 +3,22 @@
 import builtins
 import os
 from .langenv_common import get_bin, create_alias
+from builtins        import __xonsh__    # XonshSession (${...} is '__xonsh__.env')
 
 __all__ = ()
 
+base  = 'nodenv'
 NODENV = get_bin("nodenv")
 
 # check if nodenv installed
 if NODENV:
-    NODENV_ENV = $(@(NODENV) init -)
+    # Set environment
+    envx = __xonsh__.env
+    Home = envx.get("HOME")
+    NODENV_ROOT = envx.get("NODENV_ROOT")
+    if not NODENV_ROOT:
+        NODENV_ROOT = f"{Home}/.{base}"
+    envx.get("PATH").add(f'{NODENV_ROOT}/shims', front=True) # prepend shims to PATH
+    envx["NODENV_SHELL"] = "Python"
 
-    # init nodenv
-    source-bash -n --suppress-skip-message @(NODENV_ENV) e>/dev/null
-
-    create_alias("nodenv", NODENV, NODENV_ENV)
+    create_alias(base, NODENV)
